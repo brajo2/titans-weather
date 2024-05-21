17b01dca3889a6eeeb5d7e41536ad42f4e54d787
Josiah Brady <josiahbrady12@gmail.com>
2024-05-20 19:15:30 -0500

    sql jinja templates

---
+++ sql/templates/create_historical_table.j2.sql
@@ -0,0 +1,23 @@
+CREATE TABLE if not exists {{schema}}.historical_games (
+    game_id SERIAL PRIMARY KEY,
+    season INT,
+    week INT,
+    game_date DATE,
+    start_time TIME,
+    start_time_gmt_offset INT,
+    game_site text,
+    home_team text,
+    home_team_final_score INT,
+    visit_team text,
+    visit_team_final_score INT,
+    weather_hash text,
+    weather_data JSONB,
+    weather_units JSONB,
+    use_weather_variable JSONB,
+    venue_id INT,
+    venue_name text,
+    venue_geo_latitude FLOAT,
+    venue_geo_longitude FLOAT,
+    venue_surface text,
+    venue_roof_type text
+);
