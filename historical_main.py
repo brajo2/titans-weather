b028175bd9a5b66cdce65b1964d8f053528ec195
Josiah Brady <josiahbrady12@gmail.com>
2024-05-21 19:10:44 -0500

    Update historical_main.py

--- historical_main.py
+++ historical_main.py
@@ -1,14 +1,15 @@
 from typing import List
-from pipelines.historical_pipeline import enrich_venue_data, enrich_game_data, multithread_process_games, \
-    create_historical_table, insert_into_historical_table
+from pipeline.pipeline_util import multithread_process_games, enrich_venue_data, enrich_game_data, \
+    create_postgres_table, insert_into_postgres_table
 from shared.game_venue import Venue, Game
+from shared.weather_enum import Forecast
 from sql.sql_helpers import create_pg_engine
 
 
 def run_historical():
-    # create pipelines titans table
+    # create pipeline titans table
     engine = create_pg_engine()
-    create_historical_table(engine)
+    create_postgres_table(engine, is_forecast=False)
 
     # load in the primary csvs, enrich games with Venue classes
     venue_data: List[Venue] = enrich_venue_data('csv/venues.csv')
@@ -16,19 +17,21 @@ def run_historical():
 
     # get weather for each game
     # one at a time, very slow (5+ minutes)
-    # game_data = enrich_game_weather(game_data)
+    # game_data = enrich_game_weather(game_data,
+    #                weather_variables=[BASE_HOURLY_PARAMS + [HourlyWeatherParam.PRECIPITATION_PROBABILITY_FUTURE]],
+    #                weather_forecast_type=Forecast.ARCHIVE)
 
     # but could be multithreaded for speed!!
     # as there are 800+ calls to the API (takes 40 seconds instead)
-    game_data = multithread_process_games(game_data)
+    game_data = multithread_process_games(game_data, weather_forecast_type=Forecast.ARCHIVE)
 
 
-    # insert game data into pipelines titans table
+    # insert game data into pipeline titans table
     # options here, insert one by one, or in bulk
     # I think bulk is better/quicker for this example but if we ever
     # want to log each game individually to see whether it fails to insert (and why)
     # this game.insert_into_historical() would be a sensible code path
-    insert_into_historical_table(engine, game_data)
+    insert_into_postgres_table(engine, game_data, is_forecast=False)
 
 if __name__ == '__main__':
     run_historical()
